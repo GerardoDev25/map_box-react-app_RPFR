0fcd54f629b6a13d0a84b6c29094af87ca5aa697
Gerardo Miranda <gerardodev25@gmail.com>
2023-12-27 21:39:34 -0400

    chore(Maps): :sparkles: 021 Mostrar la polyline en el mapa
    021 Mostrar la polyline en el mapa
    
    021 Mostrar la polyline en el mapa

--- src/context/maps/MapProvider.tsx
+++ src/context/maps/MapProvider.tsx
@@ -1,5 +1,5 @@
 import { useReducer, JSX, useContext, useEffect } from 'react';
-import { Map, Marker, Popup } from 'mapbox-gl';
+import { AnySourceData, LngLatBounds, Map, Marker, Popup } from 'mapbox-gl';
 import { MapContext } from './MapContent';
 import { mapReducer } from './mapReducer';
 import { PlacesContext } from '..';
@@ -74,14 +74,61 @@ export const MapProvider = ({ children }: Props) => {
       `/${start.join(',')};${end.join(',')}`
     );
 
-    const { distance, duration } = resp.data.routes[0];
+    const { distance, duration, geometry } = resp.data.routes[0];
+
+    const { coordinates: coords } = geometry;
 
     let kms = distance / 1000;
     kms = Math.round(kms * 100);
     kms /= 100;
     const minutes = Math.floor(duration / 60);
-
     console.log({ kms, minutes });
+
+    const bounds = new LngLatBounds(start, start);
+
+    for (const cord of coords) {
+      const newCord: [number, number] = [cord[0], cord[1]];
+      bounds.extend(newCord);
+    }
+
+    state.map?.fitBounds(bounds, { padding: 200 });
+
+    const sourceData: AnySourceData = {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: coords,
+            },
+          },
+        ],
+      },
+    };
+
+    if (state.map?.getLayer('RouteString')) {
+      state.map?.removeLayer('RouteString');
+      state.map?.removeSource('RouteString');
+    }
+
+    state.map?.addSource('RouteString', sourceData);
+    state.map?.addLayer({
+      id: 'RouteString',
+      type: 'line',
+      source: 'RouteString',
+      layout: {
+        'line-cap': 'round',
+        'line-join': 'round',
+      },
+      paint: {
+        'line-color': 'black',
+        'line-width': 3,
+      },
+    });
   };
 
   return (
